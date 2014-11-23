a58ebcbb58dc23e390b82a8cb227c360f1bf86f1
Ryan Christiani <ryan@ryanchristiani.com>
2014-11-22 21:21:46 -0500

    Added favicon and rounded out functionality

--- js/chat.js
+++ js/chat.js
@@ -14,29 +14,57 @@ chat.socket = io.connect(chat.siteUrl);
 
 chat.userName = '';
 
-chat.router = Backbone.Router.extend({
+chat.Router = Backbone.Router.extend({
 	initialize: function() {
 		Backbone.history.start();
-		var header = new chat.header();
-		var messageBar = new chat.messageBar();
-		var chatWindow = new chat.chatWindow();
+		var header = new chat.Header();
+		var messageBar = new chat.MessageBar();
+		var login = new chat.Login();
 	},
 	routes: {
 		'about' : 'about'
 	},
 	about: function() {
-		console.log('About');
+		var aboutView = new chat.AboutView();
 	}
 });
 
+chat.Login = Backbone.View.extend({
+	el: 'body',
+	template: _.template($('#login-template').html()),
+	events: {
+		'submit .login-form' : 'createConnection'
+	},
+	initialize: function() {
+		this.render();
+	},
+	render: function() {
+		this.$el.append(this.template);
+		this.$el.find('input[type="text"]').focus();
+		return this;
+	},
+	createConnection: function(e) {
+		var input = this.$el.find('.login-container input[type="text"]');
+		e.preventDefault();
+		chat.userName = input.val();
+		if(chat.userName !== '') {
+			var chatWindow = new chat.ChatWindow();
+			this.$el.removeClass('show-login');
+			this.undelegateEvents();
+		}
+		else {
+			input.focus();
+		}
+	}
+});
 
-chat.chatWindow = Backbone.View.extend({
+chat.ChatWindow = Backbone.View.extend({
 	el: '.chat-window',
 	messageTemplate: _.template($('#message-template').html()),
 	disconnectTemplate: _.template($('#disconnect-template').html()),
 	connectedTemplate: _.template($('#connected-template').html()),
 	initialize: function() {
-		this.setUserName();
+		this.createConnection();
 	},
 	createConnection: function() {
 		var self = this;
@@ -50,11 +78,6 @@ chat.chatWindow = Backbone.View.extend({
 		chat.socket.on('userDisconnect', function(data) {
 			self.userDisconnect(data);
 		});
-	},
-	setUserName: function() {
-		chat.userName = prompt("Enter User Name: ");
-		this.createConnection();
-		return this;
 	},
 	renderMessage: function(message) {
 		this.appendMessage(message, this.messageTemplate);
@@ -72,8 +95,8 @@ chat.chatWindow = Backbone.View.extend({
 		this.$el[0].scrollTop = this.$el[0].scrollHeight;
 	}
 });
-//Message Bar 
-chat.messageBar = Backbone.View.extend({
+
+chat.MessageBar = Backbone.View.extend({
 	el: '.message-bar',
 	template: _.template($('#message-bar').html()),
 	events: {
@@ -103,7 +126,7 @@ chat.messageBar = Backbone.View.extend({
 	}
 });
 
-chat.header = Backbone.View.extend({
+chat.Header = Backbone.View.extend({
 	el: '.main-header',
 	template: _.template($('#header-template').html()),
 	initialize: function() {
@@ -114,17 +137,42 @@ chat.header = Backbone.View.extend({
 	}
 });
 
+chat.AboutView = Backbone.View.extend({
+	el: '.overlay',
+	template: _.template($('#about-container').html()),
+	events: {
+		'click .close-btn' : 'closeView'
+	},
+	initialize: function() {
+		this.render();
+	},
+	render: function() {
+		this.$el.append(this.template);
+		$('body').removeClass('show-login').addClass('show-info');
+	},
+	closeView: function(e) {
+		e.preventDefault();
+		if(chat.userName === '') {
+			$('body').addClass('show-login');
+		}
+		this.undelegateEvents();
+		this.$el.empty();
+		$('body').removeClass('show-info');
+		chat.router.navigate('/', {trigger: false});
+	}
+});
+
 chat.events = function() {
 	$(window).on('unload', function() {
 		var message = {};
 		message.time = new Date();
-		message.message = "User Disconnected";
+		message.message = chat.userName + " Disconnected";
 		chat.socket.emit('disconnected', message);
 	});
 };
 
 chat.init = function() {
-	var router = new chat.router();	
+	chat.router = new chat.Router();	
 	chat.events();
 };
 
